c648117d3b4c4aec356101c0d06227c5b93d365e
sailendrachettri <sailendra9083@gmail.com>
2023-12-20 14:20:31 +0530

    clear cache data btn moved to button and added code to remove quizof the day and current affairs data

--- js/common.js
+++ js/common.js
@@ -1,7 +1,9 @@
 function deleteCachingData() {
     let choice = confirm("You are going to delete previous content and reload!");
     if (choice) {
-        localStorage.clear();
+        localStorage.removeItem('previousHistoryData');
+        localStorage.removeItem('previousQuizzesData');
+        localStorage.removeItem('previousCurrentAffairs');
         location.reload();
     }
 }
